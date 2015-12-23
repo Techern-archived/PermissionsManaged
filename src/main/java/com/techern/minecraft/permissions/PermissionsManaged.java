4a0194591741d9577fd221ab07af634485039a2e
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-12-23 12:10:33 +1000

    Temporary core mod, it disables itself however

--- src/main/java/com/techern/minecraft/permissions/PermissionsManaged.java
+++ src/main/java/com/techern/minecraft/permissions/PermissionsManaged.java
@@ -1,18 +1,18 @@
 package com.techern.minecraft.permissions;
 
 import com.techern.minecraft.permissions.proxy.CommonProxy;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * A {@link Mod} for permissions in Minecraft
- *
- * @since 0.0.1
- */
-@Mod(modid = "PermissionsManaged", version = PermissionsManaged.VERSION)
-public class PermissionsManaged {
+import java.util.Map;
+
+@IFMLLoadingPlugin.Name(value = "Permissions Managed") // The readable mod name
+@IFMLLoadingPlugin.MCVersion(value = "1.8") // The MC version it is designed for (Remember? Upwards/Downwards compatibility lost!)
+@IFMLLoadingPlugin.TransformerExclusions(value = "com.techern.minecraft.permissions") // Your whole core mod package - Whatever you don't want the transformers to run over to prevent circularity Exceptions
+@IFMLLoadingPlugin.SortingIndex(value = 1010) // How early your core mod is called - Use > 1000 to work with srg names
+public class PermissionsManaged implements IFMLLoadingPlugin {
 
     /**
      * The instance of {@link PermissionsManaged}
@@ -42,21 +42,31 @@ public class PermissionsManaged {
      *
      * @since 0.0.1
      */
-    public static Logger LOGGER;
+    public static Logger LOGGER = LogManager.getLogger("PermissionsManaged");
 
-    /**
-     * Handles the {@link FMLPreInitializationEvent}
-     *
-     * @param event The {@link FMLPreInitializationEvent} to be handled
-     *
-     * @since 0.0.1
-     */
-    @Mod.EventHandler
-    public static void onPreInit(FMLPreInitializationEvent event) {
-        LOGGER = event.getModLog();
 
-        LOGGER.info("PermissionsManaged logging set up");
-
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
     }
 
+    @Override
+    public String getModContainerClass() {
+        return "com.techern.minecraft.permissions.PermissionsManagedModContainer";
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+        LOGGER.info("Got inject: " + data);
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
 }
